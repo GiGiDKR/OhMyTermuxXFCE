47e5e62af1c801ef3ec0916bb6a194152ce9965b
GiGiDKR <74984020+GiGiDKR@users.noreply.github.com>
2024-07-19 21:50:47 +0200

    2.0

---
+++ scripts/fishandtide.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+pkg install fish -y
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+fisher install IlanCosman/tide@v6
