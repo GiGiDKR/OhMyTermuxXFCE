541e015d041bcb97e427fc299bd3b3660aa82e9b
GiGiDKR <74984020+GiGiDKR@users.noreply.github.com>
2024-07-21 10:45:12 +0200

    Update nerdfont.sh

--- scripts/nerdfont.sh
+++ scripts/nerdfont.sh
@@ -1,6 +1,1 @@
-creating fonts folder: /data/data/com.termux/files/home/.fonts
-unzip the JetBrainsMono.zip
-Archive:  JetBrainsMono.zip
-  inflating: /data/data/com.termux/files/home/.fonts/JetBrainsMono/JetBrainsMono
-
 bash -c  "$(curl -fsSL https://raw.githubusercontent.com/officialrajdeepsingh/nerd-fonts-installer/main/install.sh)"
