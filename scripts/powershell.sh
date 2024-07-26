9db0e943843e71c696052a8878489e298a945ab0
GiGiDKR <74984020+GiGiDKR@users.noreply.github.com>
2024-07-26 14:33:08 +0200

    Update powershell.sh

--- scripts/powershell.sh
+++ scripts/powershell.sh
@@ -5,10 +5,10 @@ curl -L -o /tmp/powershell.tar.gz https://github.com/PowerShell/PowerShell/relea
 sudo mkdir -p $HOME/.config/microsoft/powershell/7
 
 # Expand powershell to the target folder
-sudo tar zxf /tmp/powershell.tar.gz -C /opt/microsoft/powershell/7
+sudo tar zxf /tmp/powershell.tar.gz -C $HOME/.config/microsoft/powershell/7
 
 # Set execute permissions
 sudo chmod +x $HOME/.config/microsoft/powershell/7/pwsh
 
 # Create the symbolic link that points to pwsh
-sudo ln -s $HOME/.configmicrosoft/powershell/7/pwsh $PREFIX/bin/pwsh
+sudo ln -s $HOME/.config/microsoft/powershell/7/pwsh $PREFIX/bin/pwsh
