d9ceb43c97f017b4b2cd7c18d7b605be8264594f
GiGiDKR <74984020+GiGiDKR@users.noreply.github.com>
2024-07-26 15:22:08 +0200

    Update xfce-min.sh

--- xfce-min.sh
+++ xfce-min.sh
@@ -55,9 +55,9 @@ echo "source $HOME/.fancybash.sh" >> $PREFIX/etc/bash.bashrc
 sed -i "326s/\\\u/$username/" $HOME/.fancybash.sh
 sed -i "327s/\\\h/termux/" $HOME/.fancybash.sh
 
-#Config
+#Autostart Conky and Flameshot
 wget https://github.com/GiGIDKR/Termux_XFCE/raw/main/config.tar.gz
 tar -xvzf config.tar.gz
 rm config.tar.gz
-rm .config/autostart/conky.desktop
-rm .config/autostart/org.flameshot.Flameshot.desktop
+chmod +x .config/autostart/conky.desktop
+chmod +x .config/autostart/org.flameshot.Flameshot.desktop
