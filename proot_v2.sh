0cd4b3c32eb8c33f89d27bbaebd1ef55ebc33d6a
GiGiDKR <74984020+GiGiDKR@users.noreply.github.com>
2024-08-17 12:21:21 +0200

    Update proot_v2.sh

--- proot_v2.sh
+++ proot_v2.sh
@@ -81,5 +81,5 @@ mkdir $PREFIX/var/lib/proot-distro/installed-rootfs/debian/home/$username/.fonts
 mkdir $PREFIX/var/lib/proot-distro/installed-rootfs/debian/home/$username/.themes/
 
 #Setup Hardware Acceleration
-pd login debian --shared-tmp -- env DISPLAY=:1.0 wget https://github.com/GiGIDKR/Termux_XFCE/raw/main/mesa-vulkan-kgsl_24.1.0-devel-20240120_arm64.deb
+pd login debian --shared-tmp -- env DISPLAY=:1.0 wget https://github.com/GiGIDKR/OhMyTermuxXFCE/raw/main/mesa-vulkan-kgsl_24.1.0-devel-20240120_arm64.deb
 pd login debian --shared-tmp -- env DISPLAY=:1.0 sudo apt install -y ./mesa-vulkan-kgsl_24.1.0-devel-20240120_arm64.deb
