9adff6fb4691b40c5ae339c0cb6651dcf8556987
GiGiDKR <74984020+GiGiDKR@users.noreply.github.com>
2024-08-20 15:43:12 +0200

    Update Gum 1.0

--- install_gum.sh
+++ install_gum.sh
@@ -1,38 +1,68 @@
 #!/bin/bash
 
-# Installation de gum
-if ! command -v gum &> /dev/null; then
-    echo "Installation de gum..."
-    pkg update -y
-    pkg install -y gum
-fi
+# Variable pour déterminer si gum doit être utilisé
+USE_GUM=false
 
-show_banner() {
-    clear
-    gum style \
-        --foreground 212 \
-        --border-foreground 212 \
-        --border double \
-        --align center \
-        --width 40 \
-        --margin "1 2" \
-        "Oh-My-Termux" \
-        "    XFCE  "
+# Vérification des arguments
+for arg in "$@"; do
+    case $arg in
+        --gum|-g)
+            USE_GUM=true
+            shift
+            ;;
+    esac
+done
+
+# Fonction pour vérifier et installer gum
+check_and_install_gum() {
+    if $USE_GUM && ! command -v gum &> /dev/null; then
+        echo "Installation de gum..."
+        pkg update -y && pkg install gum -y
+    fi
 }
 
+# Fonction pour afficher la bannière avec ou sans gum
+show_banner() {
+    clear
+    if $USE_GUM; then
+        gum style \
+            --foreground 212 \
+            --border-foreground 212 \
+            --border double \
+            --align center \
+            --width 40 \
+            --margin "1 2" \
+            "Oh-My-Termux" \
+            "    XFCE  "
+    else
+        echo "Oh-My-Termux - XFCE"
+        echo ""
+    fi
+}
+
+# Fonction de fin pour gérer les erreurs
 finish() {
   local ret=$?
   if [ ${ret} -ne 0 ] && [ ${ret} -ne 130 ]; then
     echo
-    gum style --foreground 196 "ERREUR: Installation de XFCE dans Termux impossible."
+    if $USE_GUM; then
+        gum style --foreground 196 "ERREUR: Installation de XFCE dans Termux impossible."
+    else
+        echo "ERREUR: Installation de XFCE dans Termux impossible."
+    fi
     echo "Veuillez vous référer au(x) message(s) d'erreur ci-dessus."
   fi
 }
 
 trap finish EXIT
 
+# Début du script
 clear
 
+# Appel de la fonction pour vérifier et installer gum
+check_and_install_gum
+
+# Afficher la bannière
 show_banner
 
 # Vérification et création du fichier colors.properties si nécessaire
@@ -66,74 +96,159 @@ color15=#ffffff
 EOL
 fi
 
-echo ""
-username=$(gum input --placeholder "Entrez le nom d'utilisateur à créer : ")
+show_banner
+if $USE_GUM; then
+    username=$(gum input --placeholder "Entrez le nom d'utilisateur à créer : ")
+else
+    echo "Entrez le nom d'utilisateur à créer : "
+    read username
+fi
 clear
 
-if gum confirm "Choisir un répertoire de sources Termux ?"; then
-    termux-change-repo
+show_banner
+if $USE_GUM; then
+    gum confirm "Choisir un répertoire de sources Termux ?" && termux-change-repo
+else
+    echo "Choisir un répertoire de sources Termux ? (o/n)"
+    read choice
+    [ "$choice" = "o" ] && termux-change-repo
 fi
 
 show_banner
-gum spin --title "Mise à jour des paquets" -- pkg update -y
+if $USE_GUM; then
+    gum spin --title "Mise à jour des paquets" -- pkg update -y
+else
+    echo && echo "Mise à jour des paquets..."
+    pkg update -y
+fi
+
 show_banner
-gum spin --title "Mise à niveau des paquets" -- pkg upgrade -y
+if $USE_GUM; then
+    gum spin --title "Mise à niveau des paquets" -- pkg upgrade -y
+else
+    echo && echo "Mise à niveau des paquets..."
+    pkg upgrade -y
+fi
+
 
 file_path="$HOME/.termux/termux.properties"
 
-if [ ! -f "$file_path" ]; then
-  echo "Le fichier $file_path n'existe pas."
-fi
-sed -i 's/^#\(allow-external-apps = true\)/\1/' "$file_path"
-sed -i 's/^#\(use-black-ui = true\)/\1/' "$file_path"
-sed -i 's/^#\(bell-character = ignore\)/\1/' "$file_path"
-sed -i 's/^#\(fullscreen = true\)/\1/' "$file_path"
+# Vérifier et créer le répertoire si nécessaire
+mkdir -p "$(dirname "$file_path")"
 
-if [ -f "/data/user/0/com.termux/files/usr/etc/motd" ];then
-    mv /data/user/0/com.termux/files/usr/etc/motd /data/user/0/com.termux/files/usr/etc/motd.bak;
+# Vérifier l'existence du fichier et le créer avec le contenu par défaut si nécessaire
+if [ ! -f "$file_path" ]; then
+    echo "Le fichier $file_path n'existe pas. Création du fichier avec le contenu par défaut."
+    cat <<EOL > "$file_path"
+allow-external-apps = true
+use-black-ui = true
+bell-character = ignore
+fullscreen = true
+EOL
+else
+    # Modifier le fichier pour décommenter les lignes spécifiques
+    sed -i 's/^#\(allow-external-apps = true\)/\1/' "$file_path"
+    sed -i 's/^#\(use-black-ui = true\)/\1/' "$file_path"
+    sed -i 's/^#\(bell-character = ignore\)/\1/' "$file_path"
+    sed -i 's/^#\(fullscreen = true\)/\1/' "$file_path"
+fi
+
+if [ -f "/data/user/0/com.termux/files/usr/etc/motd" ]; then
+    mv /data/user/0/com.termux/files/usr/etc/motd /data/user/0/com.termux/files/usr/etc/motd.bak
 else
     echo "Le fichier motd n'existe pas !"
 fi
 
 show_banner
-gum confirm "Accorder l'accès au stockage externe ?" && termux-setup-storage
+if $USE_GUM; then
+    gum confirm "Accorder l'accès au stockage externe ?" && termux-setup-storage
+else
+    echo "Accorder l'accès au stockage externe ? (o/n)"
+    read choice
+    [ "$choice" = "o" ] && termux-setup-storage
+fi
 
 show_banner
 pkgs=('wget' 'ncurses-utils' 'dbus' 'proot-distro' 'x11-repo' 'tur-repo' 'pulseaudio')
 
 show_banner
-gum spin --title "Installation des pré-requis" -- pkg install ncurses-ui-libs
+if $USE_GUM; then
+    gum spin --title "Installation des pré-requis" -- pkg install ncurses-ui-libs && pkg uninstall dbus -y
+else
+    echo && echo "Installation des pré-requis..."
+    pkg install ncurses-ui-libs && pkg uninstall dbus -y
+fi
 
-gum spin --title "Désinstallation de dbus" -- pkg uninstall dbus -y
 show_banner
-gum spin --title "Mise à jour des paquets" -- pkg update
+if $USE_GUM; then
+    gum spin --title "Mise à jour des paquets" -- pkg update -y
+else
+    echo && echo "Mise à jour des paquets..."
+    pkg update -y
+fi
+
 show_banner
-gum spin --title "Installation des paquets nécessaires" -- pkg install "${pkgs[@]}" -y
+if $USE_GUM; then
+    gum spin --title "Installation des paquets nécessaires" -- pkg install "${pkgs[@]}" -y
+else
+    echo && echo "Installation des paquets nécessaires..."
+    pkg install "${pkgs[@]}" -y
+fi
 
 show_banner
 echo ""
 echo "Création des répertoires utilisateur..."
-mkdir $HOME/Desktop
+mkdir -p $HOME/Desktop
+# To do : symlink
 
-gum spin --title "Téléchargement des scripts" -- wget https://github.com/GiGiDKR/OhMyTermuxXFCE/raw/main/xfce_gum.sh
-gum spin --title "Téléchargement des scripts" -- wget https://github.com/GiGiDKR/OhMyTermuxXFCE/raw/main/proot_gum.h
-gum spin --title "Téléchargement des scripts" -- wget https://github.com/GiGiDKR/OhMyTermuxXFCE/raw/main/utils.sh
+show_banner
+if $USE_GUM; then
+    gum spin --title "Téléchargement des scripts" -- wget https://github.com/GiGiDKR/OhMyTermuxXFCE/raw/main/xfce_gum.sh
+    gum spin --title "Téléchargement des scripts" -- wget https://github.com/GiGiDKR/OhMyTermuxXFCE/raw/main/proot_gum.sh
+    gum spin --title "Téléchargement des scripts" -- wget https://github.com/GiGiDKR/OhMyTermuxXFCE/raw/main/utils.sh
+else
+    clear && echo "Téléchargement des scripts..."
+    wget https://github.com/GiGiDKR/OhMyTermuxXFCE/raw/main/xfce_gum.sh
+    wget https://github.com/GiGiDKR/OhMyTermuxXFCE/raw/main/proot_gum.sh
+    wget https://github.com/GiGiDKR/OhMyTermuxXFCE/raw/main/utils.sh
+fi
 
 chmod +x *.sh
 
 show_banner
-gum spin --title "Exécution du script xfce" -- ./xfce_gum.sh "$username"
-gum spin --title "Exécution du script proot" -- ./proot_gum.sh "$username"
-gum spin --title "Exécution du script utils" -- ./utils.sh
+if $USE_GUM; then
+    gum spin --title "Exécution du script xfce" -- ./xfce_gum.sh "$username"
+    gum spin --title "Exécution du script proot" -- ./proot_gum.sh "$username"
+    gum spin --title "Exécution du script utils" -- ./utils.sh
+else
+    echo && echo "Exécution du script xfce..."
+    ./xfce_gum.sh "$username"
+    echo && echo "Exécution du script proot..."
+    ./proot_gum.sh "$username"
+    echo && echo "Exécution du script utils..."
+    ./utils.sh
+fi
 
 show_banner
-echo "Installation de Termux-X11 APK" 
+echo "Installation de Termux-X11 APK"
 echo ""
-if gum confirm "Voulez-vous installer Termux-X11 ?"; then
-    gum spin --title "Téléchargement de Termux-X11 APK" -- wget https://github.com/termux/termux-x11/releases/download/nightly/app-arm64-v8a-debug.apk
-    mv app-arm64-v8a-debug.apk $HOME/storage/downloads/
-    termux-open $HOME/storage/downloads/app-arm64-v8a-debug.apk
-    rm $HOME/storage/downloads/app-arm64-v8a-debug.apk
+if $USE_GUM; then
+    if gum confirm "Voulez-vous installer Termux-X11 ?"; then
+        gum spin --title "Téléchargement de Termux-X11 APK" -- wget https://github.com/termux/termux-x11/releases/download/nightly/app-arm64-v8a-debug.apk
+        mv app-arm64-v8a-debug.apk $HOME/storage/downloads/
+        termux-open $HOME/storage/downloads/app-arm64-v8a-debug.apk
+        rm $HOME/storage/downloads/app-arm64-v8a-debug.apk
+    fi
+else
+    echo "Voulez-vous installer Termux-X11 ? (o/n)"
+    read choice
+    if [ "$choice" = "o" ]; then
+        echo && echo "Téléchargement de Termux-X11 APK..."
+        wget https://github.com/termux/termux-x11/releases/download/nightly/app-arm64-v8a-debug.apk
+        mv app-arm64-v8a-debug.apk $HOME/storage/downloads/
+        termux-open $HOME/storage/downloads/app-arm64-v8a-debug.apk
+        rm $HOME/storage/downloads/app-arm64-v8a-debug.apk
+    fi
 fi
 
 source $PREFIX/etc/bash.bashrc
@@ -145,10 +260,21 @@ rm utils.sh
 rm install_gum.sh
 
 show_banner
-echo ""
-echo "Installation terminée !"
-echo ""
-echo "Pour lancer XFCE4, saisissez : start"
-echo "Pour accéder à DEBIAN saisissez : debian"
-echo ""
-gum style --border double --padding "1 2" --margin "1 2" --align center --foreground 212 --border-foreground=212 --width 40 "Installation terminée !"
+if $USE_GUM; then
+    if $USE_GUM; then
+        gum style \
+            --foreground 212 \
+            --border-foreground 212 \
+            --align center \
+            --width 40 \
+            --margin "1 2" \
+            "Installation terminée !" \
+            "Exécuter XFCE4 : start" \
+            "Exécuter DEBIAN : debian"
+ else
+    echo "Installation terminée !"
+    echo
+    echo "Exécuter XFCE4 : start"
+    echo "Exécuter DEBIAN : debian"
+    echo
+fi
