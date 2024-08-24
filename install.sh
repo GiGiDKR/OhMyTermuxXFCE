a2732d7695802bbfb06bcbabf370e3ec074583a4
GiGiDKR <74984020+GiGiDKR@users.noreply.github.com>
2024-08-24 16:23:50 +0200

    Update 1.0.5

--- install.sh
+++ install.sh
@@ -398,7 +398,7 @@ show_banner
 
 # Menu de séléction de police
 if $USE_GUM; then
-    FONT=$(gum choose --height=20 --header="Sélectionner la police à installer :" "Police par défaut" "CaskaydiaCove Nerd Font" "FiraMono Nerd Font" "JetBrainsMono Nerd Font" "Mononoki Nerd Font" "VictorMono Nerd Font" "RobotoMono Nerd Font" "DejaVuSansMono Nerd Font" "UbuntuMono Nerd Font" "AnonymousPro Nerd Font" "Terminus Nerd Font")
+    FONT=$(gum choose --height=14 --header="Sélectionner la police à installer :" "Police par défaut" "CaskaydiaCove Nerd Font" "FiraMono Nerd Font" "JetBrainsMono Nerd Font" "Mononoki Nerd Font" "VictorMono Nerd Font" "RobotoMono Nerd Font" "DejaVuSansMono Nerd Font" "UbuntuMono Nerd Font" "AnonymousPro Nerd Font" "Terminus Nerd Font")
 else
     echo "Sélectionner la police à installer :"
     echo "1) Police par défaut"
